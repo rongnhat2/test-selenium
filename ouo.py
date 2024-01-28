8497f897738d27432cd99da8f61b43ce80687414
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2024-01-28 19:14:23 +0700

    heasless

--- ouo.py
+++ ouo.py
@@ -5,36 +5,39 @@ from selenium.webdriver.firefox.options import Options
 import requests
 
 
-for x in range(4):
+for x in range(2):
 	url = "https://launch.rhass.vn/api/get?status=2"
 	response = requests.get(url)
 	response.encoding = 'utf-8'  
-	print("Loading page")
+	print("----------------")
+	print("* Loading page *")
 	print(response.text)
 
 	firefox_options = Options()
 	# firefox_options.set_preference("browser.privatebrowsing.autostart", True)
 	firefox_options.add_argument("start-maximized")
+	firefox_options.add_argument("-private")
 	firefox_options.add_argument("--headless")
 
 	driver = webdriver.Firefox(options=firefox_options)
 
 	driver.set_window_size(1920, 1080)
 	driver.get(response.text)
-	print("finished")
+	print("* Loaded       *")
 	time.sleep(3) 
-	print("Clicking Button")
+	print("* Run Button 1 *")
 	time.sleep(3)
-	print(driver.execute_script("document.querySelectorAll('html')"))
 	print(driver.execute_script("document.documentElement.innerHTML"))
 	time.sleep(3)
 	driver.execute_script("document.getElementsByTagName('button')[0].click()")
-	print("Clicking successful")
+	print("* Done btn 1   *")
 	time.sleep(3) 
-	print("running button 2")
+	print("* Run Button 2 *")
 	time.sleep(3)
 	driver.execute_script("document.getElementsByTagName('button')[0].click()")
 	time.sleep(3)
-	print("click successful")
-	time.sleep(20)
+	print("* Done btn 2   *")
+	print("----------------")
+	print("  ")
+	time.sleep(2)
 	driver.quit() 
